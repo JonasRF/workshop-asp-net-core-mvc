c8f063b361b8503ff07724ce87921a9f26199482
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2021-01-08 00:21:46 -0300

    Other entities and second migration

--- SalesWebMVC/Models/Departament.cs
+++ SalesWebMVC/Models/Departament.cs
@@ -1,9 +1,33 @@
-﻿
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
 namespace SalesWebMVC.Models
 {
     public class Departament
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> sellers { get; set; } = new List<Seller>();
+
+        public Departament()
+        {
+        }
+
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSallers(Seller seller)
+        {
+            sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime inicial, DateTime final)
+        {
+            return sellers.Sum(saller => saller.TotalSales(inicial, final));
+        }
     }
 }
